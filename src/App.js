a47fd224211227654bcfd76c0910017cc94c0f59
kazuhi-ra <kazuhira4126@gmail.com>
2020-05-09 22:26:36 +0900

    表示の更新をモデルを利用したものに変更

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import { element } from "./view/html-util.js";
+import { TodoListModel } from "./model/TodoListModel.js";
+import { TodoItemModel } from "./model/TodoItemModel.js";
+import { element, render } from "./view/html-util.js";
 
 console.log("App.js: loaded");
 
 export class App {
+  constructor() {
+    // 1. TodoListの初期化
+    this.todoListModel = new TodoListModel();
+  }
+
   mount() {
     const formElement = document.querySelector("#js-form");
     const inputElement = document.querySelector("#js-form-input");
     const containerElement = document.querySelector("#js-todo-list");
     const todoItemCountElement = document.querySelector("#js-todo-count");
-    let todoItemCount = 0;
+    // 2. todoListModelの状態が更新されたら表示を更新する
+    this.todoListModel.onChange(() => {
+      // TodoリストをまとめるList要素
+      const todoListElement = element`<ul />`;
+      // それぞれのTodoItem要素をtodoListElement以下へ追加する
+      const todoItems = this.todoListModel.getTodoItems();
+      todoItems.forEach((item) => {
+        const todoItemElement = element`<li>${item.title}</li>`;
+        todoListElement.appendChild(todoItemElement);
+      });
+      // containerElementの中身をtodoListElementで上書きする
+      render(todoListElement, containerElement);
+      // アイテム数の表示を更新
+      todoItemCountElement.textContent = `Todoアイテム数: ${this.todoListModel.getTotalCount()}`;
+    });
+    // 3. フォームを送信したら、新しいTodoItemModelを追加する
     formElement.addEventListener("submit", (event) => {
+      console.log('submit');
       event.preventDefault();
-      const todoItemElement = element`<li>${inputElement.value}</li>`;
-      containerElement.appendChild(todoItemElement);
-      todoItemCount++;
-      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+      this.todoListModel.addTodo(
+        new TodoItemModel({
+          title: inputElement.value,
+          completed: false,
+        })
+      );
       inputElement.value = "";
     });
   }
